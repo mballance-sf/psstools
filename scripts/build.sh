9da181fb8dd826941e29ce66ec89cd699bb52d6a
mballance <matt.ballance@gmail.com>
2015-02-10 08:34:05 -0800

    Initial addition

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+is_mingw=`uname | sed -e 's/MINGW.*$/1/'`
+
+if test "$is_mingw" = "1"; then
+  os=win32
+  ws=win32
+  eclipse=eclipsec
+else
+  os=linux
+  ws=gtk
+  eclipse=eclipse
+fi
+
+
+$ECLIPSE_HOME/$eclipse \
+    -nosplash -application org.eclipse.ant.core.antRunner \
+    --launcher.suppressErrors \
+    -buildfile build.xml      \
+    -verbose \
+    -Dos=$os -Dws=$ws -Darch=x86_64 $extra_defs build
