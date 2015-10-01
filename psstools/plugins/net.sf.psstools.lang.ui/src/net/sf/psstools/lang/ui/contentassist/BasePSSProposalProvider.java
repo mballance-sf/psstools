400daede971d892a518ea73a71a133e895185b6c
mballance <matt.ballance@gmail.com>
2015-10-01 10:54:50 -0700

    ""

--- psstools/plugins/net.sf.psstools.lang.ui/src/net/sf/psstools/lang/ui/contentassist/BasePSSProposalProvider.java
+++ psstools/plugins/net.sf.psstools.lang.ui/src/net/sf/psstools/lang/ui/contentassist/BasePSSProposalProvider.java
@@ -1,20 +1,40 @@
 package net.sf.psstools.lang.ui.contentassist;
 
+import javax.naming.CompositeName;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.xtext.Assignment;
 import org.eclipse.xtext.Group;
 import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.TypeRef;
+import org.eclipse.xtext.nodemodel.impl.CompositeNode;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 
 import net.sf.psstools.lang.pSS.PSSPackage;
+import net.sf.psstools.lang.pSS.exec_block_stmt;
 import net.sf.psstools.lang.pSS.hierarchical_id;
 import net.sf.psstools.lang.ui.contentassist.AbstractPSSProposalProvider;
 
 public class BasePSSProposalProvider extends AbstractPSSProposalProvider {
-	
+
+
+	@Override
+	public void createProposals(ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		if (context.getCurrentModel() instanceof exec_block_stmt &&
+				context.getCurrentNode() instanceof CompositeNode) {
+			// TODO: Add in exec-block content assist
+			EObject obj = context.getCurrentModel();
+			while (obj != null) {
+				System.out.println("obj: " + obj);
+				obj = obj.eContainer();
+			}
+
+		} else {
+			super.createProposals(context, acceptor);
+		}
+	};
 
 	@Override
 	public void complete_graph_bind_stmt(EObject model, RuleCall ruleCall, 
--- psstools/plugins/net.sf.psstools.lang/src/net/sf/psstools/lang/validation/BasePSSValidator.java
+++ psstools/plugins/net.sf.psstools.lang/src/net/sf/psstools/lang/validation/BasePSSValidator.java
@@ -1,14 +1,32 @@
 package net.sf.psstools.lang.validation;
 
+import java.util.Map;
+
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.validation.Check;
 
 import net.sf.psstools.lang.pSS.Model;
 import net.sf.psstools.lang.pSS.PSSPackage;
 import net.sf.psstools.lang.pSS.action_field_declaration;
+import net.sf.psstools.lang.pSS.exec_block_stmt;
 import net.sf.psstools.lang.pSS.user_defined_type;
 
 public class BasePSSValidator extends PSSValidator {
+	
 
+	@Override
+	public boolean validate(EDataType eDataType, Object value, DiagnosticChain diagnostics,
+			Map<Object, Object> context) {
+		System.out.println("--> validate");
+		boolean ret = super.validate(eDataType, value, diagnostics, context);
+		System.out.println("<-- validate");
+		
+		return ret;
+	}
+	
 	@Check
 	public void checkActionExists(action_field_declaration decl) {
 		if (decl.getDeclaration().getType() instanceof user_defined_type) {
@@ -18,6 +36,16 @@ public class BasePSSValidator extends PSSValidator {
 //					PSSPackage.eINSTANCE.getaction_field_declaration_Declaration());
 		}
 		
+	}
+	
+	@Check
+	public void checkVariableRefs(exec_block_stmt exec) {
+		System.out.println("check exec: " + exec.getBody());
+		try {
+			throw new Exception();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
